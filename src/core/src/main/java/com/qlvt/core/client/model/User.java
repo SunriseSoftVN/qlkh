ba22dbf8c036446cb36327fb995b416f7567ea51
Nguyen Duc Dung <dungvn3000@gmail.com>
2011-12-27 17:41:44 +0700

    init project

--- src/core/src/main/java/com/qlvt/core/client/model/User.java
+++ src/core/src/main/java/com/qlvt/core/client/model/User.java
@@ -17,20 +17,50 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package com.qlvt.client.client;
+package com.qlvt.core.client.model;
 
-import com.google.gwt.user.client.Window;
-import com.smvp4g.mvp.client.AbstractEntryPoint;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
- * The Class QlvtEntryPoint.
+ * The Class User.
  *
  * @author Nguyen Duc Dung
- * @since 12/27/11, 10:31 AM
+ * @since 12/27/11, 11:18 AM
  */
-public class QlvtEntryPoint extends AbstractEntryPoint {
-    @Override
-    public void onModuleLoad() {
-        Window.alert("ok");
+@Entity(name = "User")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
