65b5603e52c5ad09f1aec3bf44d4a3c764bb21bf
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-03-12 12:40:09 +0800

    Update database to v.1.0.5.

--- src/database/v.1.0.5.sql
+++ src/database/v.1.0.5.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `task` ADD `taskTypeCode` INT NOT NULL AFTER `quota`;
 ALTER TABLE `task` ADD `childTasks` TEXT NULL AFTER `taskTypeCode`;
+UPDATE task SET taskTypeCode = 3 WHERE (not (code like '%.5%')) and (taskTypeCode = 0);
+TRUNCATE TABLE `subtaskannualdetail`;
+TRUNCATE TABLE `subtaskdetail`;
+TRUNCATE TABLE `taskdetail`;
