85f5e4e5ec7fef3a2a5e20e252b29f72e0da9738
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-06-05 01:24:23 +0700

    Change database engine, using innodb, for supporting transaction.

--- src/database/v.1.0.7.sql
+++ src/database/v.1.0.7.sql
@@ -11,3 +11,10 @@ CREATE TABLE IF NOT EXISTS `systemlog` (
   `updateBy` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+ALTER TABLE `branch` ENGINE = InnoDB;
+ALTER TABLE `station` ENGINE = InnoDB;
+ALTER TABLE `subtaskannualdetail` ENGINE = InnoDB;
+ALTER TABLE `subtaskdetail` ENGINE = InnoDB;
+ALTER TABLE `task` ENGINE = InnoDB;
+ALTER TABLE `taskdetail` ENGINE = InnoDB;
+ALTER TABLE `user` ENGINE = InnoDB;
