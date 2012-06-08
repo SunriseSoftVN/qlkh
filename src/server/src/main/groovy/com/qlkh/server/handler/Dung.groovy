a8b1c3a241ccf83f33819f4b04a8d647238bdaf8
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-06-08 16:56:51 +0700

    Update CopyRight.

---
+++ src/server/src/main/groovy/com/qlkh/server/handler/Dung.groovy
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2012 - 2013 Nguyen Duc Dung (dungvn3000@gmail.com)
+ */
+
+
+
+package com.qlkh.server.handler
+
+/**
+ * The Class Dung.
+ *
+ * @author Nguyen Duc Dung
+ * @since 6/8/12, 4:16 PM
+ * 
+ */
+class Dung {
+    def test() {
+        println "dung ne";
+    }
+}
